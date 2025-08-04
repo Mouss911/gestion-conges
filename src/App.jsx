d4b12f94250e3d265cbf5dd600f839f4fe4e0263
Moussa Diatta <diattamoussa010@gmail.com>
2025-08-04 15:49:13 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline text-center mt-10">
+      Gestion Des Congés
+    </h1>
+  )
+}
+
+export default App
